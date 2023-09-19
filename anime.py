362ccba1978d872007c957c97a9ca8d36894e9a9
luisfowler02 <luisfowler02@gmail.com>
2023-09-19 11:04:35 -0400

    Made function in anime.py using jikanpy that gets info on the latest seasonal anime

--- anime.py
+++ anime.py
@@ -4,6 +4,8 @@ import time
 from termcolor import colored
 import random
 from fake_useragent import UserAgent
+from jikanpy import Jikan
+import json
 
 class Anime:
     def __init__(self, rank, title, score, genres, image_link):
@@ -85,5 +87,42 @@ class Anime:
             print(colored_text)
             print(colored('-' * 40, 'white'))
 
+
+        return anime_objects
+
+    def get_seasonal_anime():
+        anime_objects = []
+        url = 'https://api.jikan.moe/v4/seasons/now'
+        html = requests.get(url)
+        json_data = BeautifulSoup(html.content,'html.parser')
+        json_data = f'{json_data}'
+        data = json.loads(json_data)
+        # formatted_json = json.dumps(data,indent=4)
+        # print(formatted_json)
+        anime_list = data['data']
+        for anime_data in anime_list:
+            title = anime_data['title']
+            rank = anime_data['rank']
+            score = anime_data['score']
+            image_dict = anime_data['images']
+            jpg_dict = image_dict['jpg']
+            image_link = jpg_dict['image_url']
+            if score == None:
+                score = 'N/A'
+            print(f'Title: {title}')
+            print(f'Rank: {rank}')
+            genre_list = []
+            for genre in anime_data['genres']:
+                name = genre['name']
+                genre_list.append(name)
+            for theme in anime_data['themes']:
+                name_2 = theme['name']
+                genre_list.append(name_2)
+            print(f'Genres: {genre_list}')
+            print(f'Score: {score}')
+            print(f'Image Link: {image_link}')
+            print('-' * 40)
+            anime_obj = Anime(rank, title, score, genre_list, image_link)
+            anime_objects.append(anime_obj)
 
         return anime_objects
